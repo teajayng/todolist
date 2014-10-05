0c90d5b9283a8a2034618770802d4bcb5c8d877b
Terrance <terrance@teajayng.com>
2014-10-04 20:14:02 -0700

    Change js_compressor

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,7 @@
-app = angular.module('todo', ['xeditable']);
-
-app.run(function(editableOptions) {
+app = angular.module('todo', ['xeditable'])
+.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
-
-app.config(['$httpProvider', function($httpProvider) {
+})
+.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = $("meta[name=\"csrf-token\"]").attr("content");
 }]);
