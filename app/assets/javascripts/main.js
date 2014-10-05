f0789fdd81f96588cf2c1a720ddf49dcc1b9220f
Terrance <terrance@teajayng.com>
2014-10-04 19:50:55 -0700

    Try to fix dependency issue

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,9 @@
 app = angular.module('todo', ['xeditable']);
 
-app.config(['$httpProvider', function($httpProvider) {
-  $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = $("meta[name=\"csrf-token\"]").attr("content");
-}]);
-
 app.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
+
+app.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = $("meta[name=\"csrf-token\"]").attr("content");
+}]);
